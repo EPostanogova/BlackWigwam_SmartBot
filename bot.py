9d13d527b1ccbcbf8ad5e5d9dca0fd284dbae909
EPostanogova <e_postanogova@inbox.ru>
2020-11-07 01:49:08 +0300

    We're on a new branch

--- bot.py
+++ bot.py
@@ -7,3 +7,18 @@ def main():
         hello(Name)
 if __name__ == "__main__":
           main()
+
+def fibonacci():
+    a, b = 1, 1
+    while True:
+        yield a #zapominaem mesto restarta
+        a, b = b, a + b
+
+gen = fibonacci()
+for i in range(5):
+    print(next(gen))
+
+def main():
+    fibonacci()
+if __name__=="__main__":
+    main()
