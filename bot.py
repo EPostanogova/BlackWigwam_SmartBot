06d8eae393555bde7e046816e13e4b4a64321ad2
EPostanogova <e_postanogova@inbox.ru>
2020-11-14 22:07:23 +0300

    Fibonacci

--- bot.py
+++ bot.py
@@ -1,24 +1,30 @@
 
-Name = input ("Username: ")
+#Name = input ("Username: ")
 def hello(Name):
     print(f'Hello,{Name}')
 
+
+#def fibonacci():
+    #b = 1, 1
+    #while True:
+        #yield a #zapominaem mesto restarta
+        #a, b = b, a + b
+
+#gen = fibonacci()
+#for i in range(num):
+    #print(next(gen)
+
+#num=int(input("Enter quantity of namber:"))
+def fibo(num):
+    fib1=fib2=1
+    i=0
+    while i < num :
+        fib1,fib2 = fib2, fib1+fib2
+        i=i+1
+        print(fib2)
+        if i==num:
+            break
 def main():
-        hello(Name)
-if __name__ == "__main__":
-          main()
-
-def fibonacci():
-    a, b = 1, 1
-    while True:
-        yield a #zapominaem mesto restarta
-        a, b = b, a + b
-
-gen = fibonacci()
-for i in range(5):
-    print(next(gen))
-
-def main():
-    fibonacci()
+    fibo(5)
 if __name__=="__main__":
     main()
