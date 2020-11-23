acdde1c81e50f88cc9e5958aee7402949e8e535a
EPostanogova <e_postanogova@inbox.ru>
2020-11-23 22:40:54 +0300

    classFibonacci

--- bot.py
+++ bot.py
@@ -1,9 +1,19 @@
+class Fibonacci:
+    def __init__(self):
+        pass
+    def hello(self, name):
+        print(f'Hello', name)
 
-Name = input ("Username: ")
-def hello(Name):
-    print(f'Hello,{Name}')
-
-def main():
-        hello(Name)
-if __name__ == "__main__":
-          main()
+    def fibo(self, num):
+        fib1 = fib2 = 1
+        i = 0
+        while i < num:
+            fib1, fib2 = fib2, fib1 + fib2
+            i = i + 1
+            print(fib2)
+            if i == num:
+                break
+if __name__ =='__main__' :
+    myfibo= Fibonacci()
+    myfibo.hello('Bob')
+    myfibo.fibo(3)
