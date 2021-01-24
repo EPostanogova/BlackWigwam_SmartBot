82f02f46998f5b289ec253e40d0792a522178903
EPostanogova <e_postanogova@inbox.ru>
2021-01-24 20:19:33 +0300

    Photo

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger ('SmartBot')
+logger = logging.getLogger('SmartBot')
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 
+from stickers import stickers
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
@@ -21,22 +22,29 @@ async def process_start_command(message: types.Message):
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("start - краткая информация о боте \n help - список команд с описанием")
+    await message.reply("start - краткая информация о боте \n help - список команд с описанием \n photo - получение мгновенной фотографии вигвама")
 
 
 @dp.message_handler()
 async def hello_response(msg: types.Message):
     if 'привет' in msg.text.lower():
-        await bot.send_message(msg.from_user.id, f"Здравствуй,{msg.from_user.first_name}!")
+        await bot.send_message(msg.from_user.id, f"Здравствуй, {msg.from_user.first_name}!")
+        await bot.send_sticker(msg.chat.id, sticker=stickers["Puppy"])
     elif 'пока' in msg.text.lower():
-        await bot.send_message(msg.from_user.id, f"Прощай,{msg.from_user.first_name}!")
+        await bot.send_message(msg.from_user.id, f"Прощай, {msg.from_user.first_name}!")
+        await bot.send_sticker(msg.chat.id, sticker=stickers["Sad"])
     elif 'до свидания' in msg.text.lower():
-        await bot.send_message(msg.from_user.id, f"До новых встречь,{msg.from_user.first_name}!")
+        await bot.send_message(msg.from_user.id, f"До новых встреч, {msg.from_user.first_name}!")
+        await bot.send_sticker(msg.chat.id, sticker=stickers["LoveBye"])
+
 
 @dp.message_handler(content_types=["sticker"])
-async def sticker (message: types.Message):
-    await message.reply("Крутой стикер! Я скоро тоже смогу их отправлять!")
 
-    
+
+async def sticker(message: types.Message):
+    print(message.sticker)
+    await message.reply("Крутой стикер!")
+    await bot.send_sticker(message.chat.id, sticker=stickers["Like"])
+
 if __name__ == '__main__':
     executor.start_polling(dp)
