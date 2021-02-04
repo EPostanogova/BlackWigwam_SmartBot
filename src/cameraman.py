f055d403ec2cfb817769e597f3a3fc288e13273a
EPostanogova <e_postanogova@inbox.ru>
2021-02-04 23:20:39 +0300

    photo

--- src/cameraman.py
+++ src/cameraman.py
@@ -1,30 +1,28 @@
 import cv2
-
-class Cameraman:
-    def __init__(logging):
-        logging.basicConfig(
+import logging
+logging.basicConfig(
             level=logging.INFO,
             format='%(asctime)s : %(levelname)s : %(message)s',
             filemode='w',
         )
+class Cameraman:
+    def __init__(self):
+        self.logger = logging.getLogger('Camera')
+        self.logger.info('объект Cameraman был успешно создан')
 
-        logging.info("объект Cameraman был успешно создан")
-
-        #self.logger = logging.getLogger('Camera')
-        #self.logger.setLevel(logging.INFO)
-        #self.logger.info('объект Cameraman был успешно создан')
     def get_image(self):
         cap = cv2.VideoCapture(0)
-        while True:
-            ret, img = cap.read()
-            cv2.imshow("camera", img)
-            if cv2.waitKey(10) == 27:  # Клавиша Esc
-                break
+        ret, img = cap.read()
+        cv2.imshow("camera", img)
+
+        #if cv2.waitKey(10) == 27:  # Клавиша Esc
+
+        cv2.waitKey(10)
         cap.release()
         cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    test = Cameraman("test")
+    test = Cameraman()
     test.get_image()
 
 
