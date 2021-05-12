9fe0f7019d1f68066b5eccd46d75e9205bb502b4
EPostanogova <64282789+EPostanogova@users.noreply.github.com>
2021-05-12 16:44:13 +0300

    Пустая строчка

--- src/arduino.py
+++ src/arduino.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import logging
+import datetime
 
 class ArduinoConnector:
     def __init__(self,com_port,baudrate):
@@ -8,13 +9,19 @@ class ArduinoConnector:
         self.logger = logging.getLogger('Arduino')
         self.com_port=com_port
         self.baudrate=baudrate
-        self.ser = serial.Serial(com_port,baudrate,timeout=5)
+        self.ser = serial.Serial(com_port,baudrate,timeout=1)
+
 
     def echo(self,msg):
-
         self.ser.write(str.encode(msg))
-        time.sleep(1)
-        msg_echo= self.ser.read(self.ser.inWaiting())
+        msg_echo = ""
+        start_time = datetime.datetime.now()
+        timeout=3
+        while len(msg_echo) == 0:
+            msg_echo = self.ser.read(self.ser.inWaiting())
+            cur_time = datetime.datetime.now()
+            if (cur_time - start_time).total_seconds() >= timeout * 60:
+                break
         self.ser.close()
-        return  msg_echo
+        return msg_echo.decode("utf-8")
 
